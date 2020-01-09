bba275ff3ebd9c3ea9177c6fc2249f6b593db3f0
Arghya Pal <palarghya337@gmail.com>
2020-01-09 10:51:22 +0530

    Added more interview questions and example for init and destroy methods

---
+++ src/main/java/com/spring/practice/bean/InitAndDestroyBeanImpl.java
@@ -0,0 +1,26 @@
+package com.spring.practice.bean;
+
+import org.springframework.beans.factory.DisposableBean;
+import org.springframework.beans.factory.InitializingBean;
+import org.springframework.stereotype.Component;
+
+import com.spring.practice.utils.Log;
+
+@Component
+public class InitAndDestroyBeanImpl implements InitializingBean, DisposableBean {
+
+	public InitAndDestroyBeanImpl() {
+		Log.logMessage("InitAndDestroyBeanImpl() constructor");
+	}
+	@Override
+	public void afterPropertiesSet() throws Exception {
+		Log.logInfo("afterPropertiesSet() method of {0} class executed.",
+				getClass().getSimpleName());
+	}
+
+	@Override
+	public void destroy() throws Exception {
+		Log.logInfo("destroy() method of {0} class executed",
+				getClass().getSimpleName());
+	}
+}
